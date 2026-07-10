1b8a2b245ea2c49ce17c37880fb55f05093a46a9
bunyipbluegum <pr@webnetics.com>
2026-07-10 17:20:30 -0600

    asd

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Phil-a-Word SW v43 | 20260710
+// Phils-a-Word SW v43 | 20260710
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
